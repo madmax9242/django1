07a5fbb152a16ea138f9840886feac12b6f48c70
Max DePriest <maxd924@gmail.com>
2015-10-12 18:27:46 -0500

    trying to get questions to show

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
+
+from .models import Question 
 
 def index(request):
-	return HttpResponse("Hello, world. You're at the polls index")
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	context = { 'latest_question_list': latest_question_list })
+	return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
 	return HttpResponse("You're looking at question %s." % question_id)
@@ -14,4 +18,10 @@ def results(request, question_id):
 
 def vote(request, question_id):
 	return HttpResponse("You're voting on question %s." % question_id)
-# Create your views here.
+
+def detail(request, question_id):
+	try:
+		question = Question.objects.get(pk=question_id)
+	except Question.DoesNotExist:
+		raise Http404("Question doest not exist")
+	return render(request, 'polls/detail.html', {'question': question})
